3580cd9c2eb173cd55da6d0570ec2ce2f387cd35
Camilo Monsalve Gomez <beren.monsalve@gmail.com>
2013-03-07 11:12:52 -0500

    without direct_to_template migration django1.5

--- contenidos/modulos/contactos/urls.py
+++ contenidos/modulos/contactos/urls.py
@@ -1,7 +1,6 @@
 from django.conf.urls.defaults import *
 from django.contrib import admin
 from django.conf import settings
-from django.views.generic.simple import direct_to_template
 from django.conf.urls.defaults import *
 from django.contrib import databrowse
 from django.contrib.auth.decorators import login_required
